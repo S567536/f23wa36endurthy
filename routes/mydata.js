96df3660d523bf895bbe013017d1c4b4a8121943
S567536 <S567536@nwmissouri.edu>
2023-10-24 11:50:33 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Rishik TReddy Endurthy' });
+  res.render('mydata', { title: 'Rishik Reddy Endurthy' });
 });
 
 module.exports = router;
